4862cb289d5561268cea95e93aeaabe749e7627d
Sirisak Lueangsaksri <spywhere@me.com>
2015-02-16 03:53:35 +0700

    Start VNWe v3 development

--- scripts/demo.js
+++
@@ -1,7 +0,0 @@
-//VN Script
-addText("~clr~ยินดีต้อนรับสู่ VNWe. ระบบ Visual Novel บนเว็บ...",30);
-wait();
-addText("~clr~คุณสามารถแก้ไขเนื้อหาได้จากไฟล์ demo.js ในโฟลเดอร์ scripts ได้ทันที...",30);
-wait();
-addText("~clr~โปรดเข้าเว็บไซต์ http://vnwe.my3gb.com/ สำหรับข้อมูลเพิ่มเติมเกี่ยวกับการใช้งาน.",30);
-wait();
