0762144b126b376ebe704944cca39a97d126c8b7
Sirisak Lueangsaksri <spywhere@me.com>
2015-02-23 13:23:32 +0700

    Add license and remove comment in output files

--- make.js
+++ make.js
@@ -11,6 +11,7 @@ fs.readdir(sourceDir, function(err, files){
 		throw err;
 	}
 	console.log("Packing code ...");
+	var license = "/** @license\n" + fs.readFileSync("LICENSE") + "*/\n\n";
 	var packedCode = "";
 
 	packedCode += "(window)=>{";
@@ -21,16 +22,16 @@ fs.readdir(sourceDir, function(err, files){
 	packedCode += "}(window);";
 
 	console.log("Compiling code ...");
-	var result = babel.transform(packedCode);
+	var result = babel.transform(packedCode, {comments: false});
 	var outputPath = path.join(outputDir, "vnwe");
-	fs.writeFile(outputPath+".js", result.code, function(err){
+	fs.writeFile(outputPath+".js", license+result.code, function(err){
 		if(err){
 			throw err;
 		}
 		console.log("Code has been compiled into vnwe.js !");
 	});
 	var minifiedCode = uglify.minify(result.code, {fromString: true}).code;
-	fs.writeFile(outputPath+".min.js", minifiedCode, function (err) {
+	fs.writeFile(outputPath+".min.js", license+minifiedCode, function(err){
 		if(err){
 			throw err;
 		}
